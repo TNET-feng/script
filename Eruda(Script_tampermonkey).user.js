48904328a6fc1af5f5ae67665003d3783846b3c0
刘宝峰 <lbfliubaofeng@gmail.com>
2026-08-25 23:11:04 +0800

    更新 Eruda(Script_tampermonkey).user.js

--- Eruda(Script_tampermonkey).user.js
+++ Eruda(Script_tampermonkey).user.js
@@ -13,64 +13,33 @@
 // ==/UserScript==
 
 (function() {
-
     'use strict';
-
     if (window.eruda || document.getElementById('eruda-script')) {
-
         return;
-
     }
-
     var script = document.createElement('script');
     script.id = 'eruda-script';
     script.src = 'https://cdn.jsdelivr.net/npm/eruda';
-
     script.onload = function() {
-
         if (typeof eruda !== 'undefined') {
-
             eruda.init();
-
             var style = document.createElement('style');
-
             style.textContent = `
-
                 .eruda-container {
-
-                    z-index: 2147483647 !important; /* 最大z-index值 */
-
-                    position: fixed !important;
-
-                }
-
-                .eruda-dev-tools {
-
-                    z-index: 2147483646 !important;
-
-                }
-
-                .eruda-entry-btn {
-
                     z-index: 2147483647 !important;
-
                     position: fixed !important;
-
                 }
-
+                .eruda-dev-tools {
+                    z-index: 2147483646 !important;
+                }
+                .eruda-entry-btn {
+                    z-index: 2147483647 !important;
+                    position: fixed !important;
+                }
             `;
-
             document.head.appendChild(style);
-
-
-
             console.log('Eruda is running!');
-
         }
-
     };
-
-
     document.body.appendChild(script);
-
 })();
