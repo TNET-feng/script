b6a7886dac9b8ee5d2bfb13de72b7921c88e1e9b
刘宝峰 <lbfliubaofeng@gmail.com>
2026-08-25 23:11:20 +0800

    更新 Eruda(Script_tampermonkey).user.js

--- Eruda(Script_tampermonkey).user.js
+++ Eruda(Script_tampermonkey).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Eruda
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Eruda
 // @author       FengPwner
 // @match        *://*/*
